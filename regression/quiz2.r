ab8049b6dfb2cd474850a17e65f4f9608d1109fa
Nick Krabbenhoeft <n@nkrabben.com>
2015-03-08 22:34:09 -0400

    Update for March 15 class

--- regression/quiz2.r
+++ regression/quiz2.r
@@ -4,12 +4,21 @@ y <- c(0.67, 0.84, 0.6, 0.18, 0.85, 0.47, 1.1, 0.65, 0.36)
 summary(lm(y ~ x))
 
 #q3
-fit <- summary(lm(mtcars$mpg ~ mtcars$wt))$coefficients 
-fit[1,1] + c(-1, 1) * qt(.975, lm(mtcars$mpg ~ mtcars$wt)$df) * fit [1,2]
+fit <- lm(mpg ~ I(wt-mean(wt)), mtcars)
+fit.summ <- summary(fit)$coefficients
+fit.summ[1,1] + c(-1, 1) * qt(.975, fit$df) * fit.summ[1,2]
 
 #q5
 fit <- lm(mpg ~ wt, mtcars)
-predict(fit, newdata=data.frame(wt=mean(mtcars$wt)), interval = 'prediction', level=0.95)
+predict(fit, data.frame(wt = 3), interval = 'prediction', level=0.95)
+
+#q6
+#q3
+fit <- lm(mpg ~ I(wt-mean(wt)), mtcars)
+fit.summ <- summary(fit)$coefficients
+fit.summ[2,1] + c(-1, 1) * qt(.975, fit$df) * fit.summ[2,2]
 
 #q9
-cor(mtcars$mpg, mtcars$wt)^2
+fit <- summary(lm(mpg ~ wt, mtcars))
+fit2 <- summary(lm(mpg ~ 1, mtcars))
+sum(fit$residuals^2)/sum(fit2$residuals^2)
